38a57c4801e91266ce034a95e5c01d944f828b83
MUmerYasin <mumeryasin123456789@gmail.com>
2021-08-12 11:40:16 +0500

    Circle Avatar add with BorderRadius 10, and Border custom green color and add ClipRRect to convert it circular to square box with rounded corner.

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'selection',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.grey,
         textTheme: GoogleFonts.playfairDisplayTextTheme(
-          Theme.of(context).textTheme,),
+          Theme.of(context).textTheme,
+        ),
       ),
       home: const MyHomePage(title: 'PROFILE'),
     );
@@ -35,7 +35,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,26 +42,60 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(
           widget.title,
           style: GoogleFonts.playfairDisplay(
-        textStyle: const TextStyle(color: appBarColorCustom, ),
-      ),
+            textStyle: const TextStyle(
+              color: appBarColorCustom,
+            ),
+          ),
         ),
-        elevation:1.0,
+        elevation: 1.0,
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:  <Widget>[
-            TextButton.icon(
-              icon: const Icon(Icons.camera),
-              label: const  Text(logOutString),
-              onPressed: () {},
-            )
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextButton.icon(
+                  icon: const Icon(
+                    Icons.logout,
+                    color: Colors.red,
+                  ),
+                  label: const Text(logOutString),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 60.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: greenIconColorCustom,
+                        width: 2,
+                      ),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                    child: ClipRRect(
+                      child: Image.asset('lib/assets/images/222.png'),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
-
     );
   }
 }
